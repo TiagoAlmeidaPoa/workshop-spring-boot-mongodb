389cd5296f2f3d36293e17e20e2574b8b68ac547
@TiagoAlmeidaPoa <tiago.pipinha@gmail.com>
2021-01-22 13:43:40 -0300

    Inserção de usuário com POST

--- src/main/java/com/tiagoalmeida/workshopmongo/services/UserService.java
+++ src/main/java/com/tiagoalmeida/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tiagoalmeida.workshopmongo.domain.User;
+import com.tiagoalmeida.workshopmongo.dto.UserDto;
 import com.tiagoalmeida.workshopmongo.repository.UserRepository;
 import com.tiagoalmeida.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,15 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("User not found by id"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDto objDto) {
+		User user = new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+		return user;
 	}
 
 }
